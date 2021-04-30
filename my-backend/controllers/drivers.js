51b7d68b71b9c3d24999333b61644c09d4706e74
Luciano Archidiacono <lucianoarchidiacono@gmail.com>
2021-04-29 23:39:18 -0300

    Ahora se ingresa un valor en el campo ACTIVE, se incorporo borrado logico

--- my-backend/controllers/drivers.js
+++ my-backend/controllers/drivers.js
@@ -1,14 +1,14 @@
 const { prepareConnection } = require("../helpers/connectionDB.js");
 const { validateDrivers } = require("../helpers/validateUserInputs.js");
 const { validateDriverTransportDependence } = require('../helpers/validateDriverDependences.js');
-const {DRIVER_ROLE} = require('../const/config.js');
+const {DRIVER_ROLE, NO_ACTIVE, ACTIVE} = require('../const/config.js');
 // const { validateDriverTripDependence } = require('../helpers/validateDriverDependences.js');
 
 const getDrivers = async (req, res) => {
     const { start = 1, limit = 5 } = req.query;
     try {
         const connection = await prepareConnection();
-        const sqlSelect = 'SELECT USER_ID, NAME, SURNAME, EMAIL FROM user a INNER JOIN role_user r ON (a.USER_ID = r.ID_USER) WHERE r.ID_ROLE = ? ORDER BY SURNAME ASC, NAME ASC LIMIT ?, ?';
+        const sqlSelect = 'SELECT USER_ID, NAME, SURNAME, EMAIL, ACTIVE FROM user a INNER JOIN role_user r ON (a.USER_ID = r.ID_USER) WHERE r.ID_ROLE = ? ORDER BY SURNAME ASC, NAME ASC LIMIT ?, ?';
         const [rows] = await connection.execute(sqlSelect, [DRIVER_ROLE, start - 1, limit]);
         connection.end();
         return res.status(200).send(rows);
@@ -33,9 +33,8 @@ const postDriver = async (req, res) => {
             const [rows] = await connection.execute(sqlInsert, [names, surname, email, password1, phoneNumber]);
 
             const id = rows.insertId;
-            sqlInsert = 'INSERT INTO role_user (ID_ROLE, ID_USER) VALUES (?,?)'
-            connection.execute(sqlInsert, [DRIVER_ROLE, id]);
-
+            sqlInsert = 'INSERT INTO role_user (ID_ROLE, ID_USER, ACTIVE) VALUES (?,?,?)'
+            connection.execute(sqlInsert, [DRIVER_ROLE, id, ACTIVE]);
             connection.end();
             res.status(201).send("Se ha registrado el chofer con exito");
         } catch (error) {
@@ -50,7 +49,7 @@ const getDriverById = async (req, res) => {
     try {
         const { id } = req.params;
         const connection = await prepareConnection();
-        const sqlSelect = 'SELECT USER_ID, NAME, SURNAME, EMAIL, PASSWORD, PHONE_NUMBER FROM user a INNER JOIN role_user r ON (a.USER_ID = r.ID_USER) WHERE r.ID_ROLE = ? AND a.USER_ID = ?';
+        const sqlSelect = 'SELECT USER_ID, NAME, SURNAME, EMAIL, PASSWORD, PHONE_NUMBER, ACTIVE FROM user a INNER JOIN role_user r ON (a.USER_ID = r.ID_USER) WHERE r.ID_ROLE = ? AND a.USER_ID = ?';
         const [rows, fields] = await connection.execute(sqlSelect, [DRIVER_ROLE, id]);
         connection.end();
         return res.status(200).send(rows[0]);
@@ -96,11 +95,8 @@ const deleteDriver = async (req, res) => {
     else {
         try {
             const connection = await prepareConnection();
-            let sqlDelete = 'DELETE FROM ROLE_USER WHERE ID_USER = ?';
-            let [rows] = await connection.execute(sqlDelete, [id]);
-
-            sqlDelete = 'DELETE FROM USER WHERE USER_ID = ?';
-            [rows] = await connection.execute(sqlDelete, [id]);
+            const sqlUptate = 'UPDATE role_user SET ACTIVE= ? WHERE ID_USER = ?';
+            const [rows] = await connection.execute(sqlUptate, [NO_ACTIVE,id]);
             connection.end();
             return res.status(200).send('Se ha eliminado el chofer con exito');
         } catch (error) {
