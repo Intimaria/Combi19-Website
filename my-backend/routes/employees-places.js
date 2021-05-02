3a498adfbd895a4a5355a17cd4d56b643be63c7d
Intimaroia <intimaria@gmail.com>
2021-05-02 18:55:28 -0300

    mas lugares -> places

---
+++ my-backend/routes/employees-places.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+
+const { getPlaces, postPlace, getPlaceById, putPlace, deletePlace } = require("../controllers/places");
+
+const { authenticateAdminRol } = require("../middlewares/authorization.js");
+
+
+const router = Router();
+
+// Retrieve all lugares
+router.get('/', authenticateAdminRol, getPlaces);
+
+// Create a new lugares
+router.post('/', authenticateAdminRol, postPlace);
+
+// Retrieve a single lugares by id
+router.get('/:id', authenticateAdminRol, getPlaceById);
+
+// Update a lugares by id
+router.put('/:id', authenticateAdminRol, putPlace);
+
+// Delete a lugares by id
+router.delete('/:id', authenticateAdminRol, deletePlace);
+
+module.exports = router;
