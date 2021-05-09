6353b159ef066da5c0487108acf0961e678a04e5
Bruno Jair Barros <brunojairbarros@hotmail.com>
2021-05-09 14:34:25 -0300

    Se restaura archivo messages.js del front

---
+++ my-frontend/src/const/messages.js
@@ -0,0 +1,62 @@
+// General messages
+export const ERROR_MSG_INTERNET = 'verifique su conexión a internet';
+// Passenger registration form
+export const ERROR_MSG_EMPTY_DATE = '* Ingrese una fecha';
+export const ERROR_MSG_EMPTY_EMAIL = '* Ingrese un correo electrónico';
+export const ERROR_MSG_EMPTY_NAME = '* Ingrese un nombre';
+export const ERROR_MSG_EMPTY_PASSWORD = '* Ingrese una contraseña';
+export const ERROR_MSG_EMPTY_REPEAT_PASSWORD = '* Ingrese la contraseña nuevamente';
+export const ERROR_MSG_EMPTY_SURNAME = '* Ingrese un apellido';
+export const ERROR_MSG_INVALID_AGE = '* Debe ser mayor de 18 años';
+export const ERROR_MSG_INVALID_DATE = '* La fecha ingresada en inválida';
+export const ERROR_MSG_INVALID_EMAIL = '* El correo electrónico posee un formato inválido';
+export const ERROR_MSG_INVALID_NAME = '* El nombre debe poseer solo caracteres alfabéticos';
+export const ERROR_MSG_INVALID_PASSWORD_NO_CAPITAL_LETTERS = '* La contraseña no posee letras mayúsculas';
+export const ERROR_MSG_INVALID_PASSWORD_NO_LOWER_CASE = '* La contraseña no posee letras minúsculas';
+export const ERROR_MSG_INVALID_PASSWORD_NO_MIN_CHARACTERS = '* La contraseña debe tener mas de 6 caracteres';
+export const ERROR_MSG_INVALID_PASSWORD_NO_NUMBERS = '* La contraseña no posee números';
+export const ERROR_MSG_INVALID_SURNAME = '* El apellido debe poseer solo caracteres alfabéticos';
+export const ERROR_MSG_PASSWORD_NO_MATCH = '* Las contraseñas no coinciden';
+// Passenger API messages
+export const OK_MSG_API_USER_POST = 'Se creó el usuario con éxito';
+export const ERROR_MSG_API_POST_USER = 'Ocurrió un error al insertar el usuario:';
+// Login API messages
+export const ERROR_MSG_API_LOGIN = 'Ocurrió un error al iniciar sesión:';
+// Driver registration form
+export const ERROR_MSG_EMPTY_PHONE_NUMBER = "* Ingrese un numero de telefono";
+export const ERROR_MSG_INVALID_PHONE_NUMBER = "* El ingrese un numero de telefono valido";
+// Drivers API messages
+export const ERROR_MSG_API_GET_DRIVERS = 'Ocurrió un error al obtener los choferes:';
+export const ERROR_MSG_API_GET_DRIVER_BY_ID = 'Ocurrió un error al obtener al chofer indicado:';
+export const ERROR_MSG_API_GET_DRIVERS_CUSTOM_AVAILABLE = 'Ocurrió un error al obtener las combis disponibles:';
+export const OK_MSG_API_POST_DRIVER = 'Se registró el chofer con éxito';
+export const ERROR_MSG_API_POST_DRIVER = 'Ocurrió un error al crear el chofer:';
+export const OK_MSG_API_PUT_DRIVER = 'Se actualizaron los datos del chofer con éxito';
+export const ERROR_MSG_API_PUT_DRIVER = 'Ocurrió un error al actualizar los datos del chofer:';
+export const OK_MSG_API_DELETE_DRIVER = 'Se eliminó el chofer con éxito';
+export const ERROR_MSG_API_DELETE_DRIVER = 'Ocurrió un error al eliminar el chofer indicado';
+export const ERROR_MSG_API_DELETE_DRIVER_TRANSPORT_DEPENDENCE = 'No se puede eliminar, el chofer figura como conductor de combis';
+// Places API messages
+export const OK_MSG_API_LOCATION_POST = 'Se creó el lugar con éxito';
+// Transport registration form
+export const ERROR_MSG_EMPTY_DRIVER = '* Seleccione un chofer';
+export const ERROR_MSG_EMPTY_INTERNAL_IDENTIFICATION = '* Ingrese una identificación interna';
+export const ERROR_MSG_EMPTY_MODEL = '* Ingrese un modelo';
+export const ERROR_MSG_EMPTY_REGISTRATION_NUMBER = '* Ingrese una patente';
+export const ERROR_MSG_INVALID_REGISTRATION_NUMBER = '* La patente ingresada es inválida';
+export const ERROR_MSG_EMPTY_SEATING = '* Ingrese la cantidad de asientos';
+export const ERROR_MSG_INVALID_MAX_SEATING = '* El máximo son 99 asientos';
+export const ERROR_MSG_INVALID_MIN_SEATING = '* El mínimo es un asiento';
+export const ERROR_MSG_INVALID_VALUE_SEATING = '* Sólo se permite valores numéricos';
+export const ERROR_MSG_EMPTY_TYPE_COMFORT = '* Seleccione un tipo confort';
+// Transports API messages
+export const ERROR_MSG_API_GET_TRANSPORTS = 'Ocurrió un error al obtener las combis:';
+export const ERROR_MSG_API_POST_TRANSPORT = 'Ocurrió un error al crear la combi:';
+export const ERROR_MSG_API_PUT_TRANSPORT = 'Ocurrió un error al actualizar los datos de la combi:';
+export const ERROR_MSG_API_DELETE_TRANSPORT = 'Ocurrió un error al eliminar la combi indicada';
+// Places
+export const ERROR_MSG_API_DELETE_PLACES = " ";
+export const ERROR_MSG_API_GET_PLACES =  " ";
+export const ERROR_MSG_API_GET_PROVINCES = " ";
+export const ERROR_MSG_API_POST_PLACES = " ";
+export const ERROR_MSG_API_PUT_PLACES = " ";
