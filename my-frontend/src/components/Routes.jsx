2b6dc6714f0634b63315a082d5d11413a9b044f3
Bruno Jair Barros <brunojairbarros@hotmail.com>
2021-05-10 20:40:29 -0300

    Se cambió la librería material-table a material-table/core

--- my-frontend/src/components/Routes.jsx
+++ my-frontend/src/components/Routes.jsx
@@ -26,7 +26,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import InputLabel from '@material-ui/core/InputLabel';
-import MaterialTable from "material-table";
+import MaterialTable from '@material-table/core';
 import { Message } from "./Message";
 import Tooltip from '@material-ui/core/Tooltip';
 import Visibility from '@material-ui/icons/Visibility';
