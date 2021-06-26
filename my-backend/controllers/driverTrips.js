4c2f1a934468bf8d214ca1a271506236ddae5546
Intimaroia <intimaria@gmail.com>
2021-06-25 21:10:03 -0300

    ordenacion de driver trips - fix

--- my-backend/controllers/driverTrips.js
+++ my-backend/controllers/driverTrips.js
@@ -16,7 +16,7 @@ const getDriverTrips = async (req, res) => {
       const connection = await prepareConnection();
       const sqlSelect = `
                         SELECT DISTINCT
-                        TRIP_ID, REPLACE(TR.PRICE, '.', ',') PRICE,
+                        TRIP_ID, REPLACE(TR.PRICE, '.', ',') PRICE, TR.DEPARTURE_DAY,
                         DATE_FORMAT(TR.DEPARTURE_DAY, '%Y-%m-%d %H:%i') DEPARTURE_DAY, R.DURATION,
                         CONCAT(DATE_FORMAT(ADDTIME(TR.DEPARTURE_DAY, R.DURATION), '%d/%m/%Y %H:%i'), 'hs') ARRIVAL_DAY,
                         CI.CITY_ID DEPARTURE_ID, CONCAT(CI.CITY_NAME, ', ', PI.PROVINCE_NAME) DEPARTURE, 
