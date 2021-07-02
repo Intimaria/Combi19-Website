fb49a3c6da2f01334254ed175a40962eb41c4d33
Intimaroia <intimaria@gmail.com>
2021-07-02 19:25:11 -0300

    export cancel trip

--- my-frontend/src/api/DriverTrips.js
+++ my-frontend/src/api/DriverTrips.js
@@ -97,3 +97,32 @@ export const finishTrip = async (id) => {
         }
     }
 }
+
+export const cancelTrip = async (id) => {
+    const token = localStorage.getItem('token');
+    try {
+        let response = await axios.put(`${BACKEND_URL}/pendingTrips/custom/canceltrip/${id}`,
+        {},
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        return response;
+    } catch (error) {
+        if (error.response?.status) {
+            return error.response;
+        } else {
+            // In this situation, is NOT an axios handled error
+
+            console.log(`${ERROR_MSG_API_FINISH_TRIP} ${error}`);
+
+            if (error.message === 'Network Error') {
+                error.message = ERROR_MSG_INTERNET;
+                return error.message;
+            } else {
+                return error.message;
+            }
+        }
+    }
+}
