6d673d2ce9e3cce00e18433a13a6c40688d04f22
Intimaroia <intimaria@gmail.com>
2021-07-03 02:05:10 -0300

    cambios

--- my-backend/controllers/driverTrips.js
+++ my-backend/controllers/driverTrips.js
@@ -64,7 +64,7 @@ const getUnsoldDriverTrips = async (req, res) => {
       const connection = await prepareConnection();
       const sqlSelect = `
                         SELECT DISTINCT 
-                        TR.TRIP_ID, REPLACE(TR.PRICE, '.', ',') PRICE, TR.DEPARTURE_DAY,
+                        TR.TRIP_ID, CONCAT('$', REPLACE(TR.PRICE, '.', ',')) PRICE, TR.DEPARTURE_DAY,
                         DATE_FORMAT(TR.DEPARTURE_DAY, '%Y-%m-%d %H:%i') DEPARTURE_DAY, R.DURATION,
                         DATE_FORMAT(ADDTIME(TR.DEPARTURE_DAY, R.DURATION), '%Y-%m-%d %H:%i') ARRIVAL_DAY,
                         CI.CITY_ID DEPARTURE_ID, CONCAT(CI.CITY_NAME, ', ', PI.PROVINCE_NAME) DEPARTURE, 
