43eb072a97a6560d940cfe5730e46a6d8ab94a96
Intimaroia <intimaria@gmail.com>
2021-07-05 13:39:53 -0300

    order by added, console.log removed

--- my-backend/controllers/reports.js
+++ my-backend/controllers/reports.js
@@ -20,13 +20,13 @@ const getRiskyPassengers = async (req, res) => {
                 WHERE R.ID_ROLE = 3
                 AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -1 MONTH) <= NOW()
                 AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -1 MONTH) >= DATE_ADD(NOW(), INTERVAL -1 MONTH)
+                ORDER BY  FECHA_REVISADO
                 `;
           // AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -1 MONTH) >= DATE_ADD(NOW(), INTERVAL -1 MONTH)
         const [rows] = await connection.execute(sqlSelect, []);
         
         connection.end();
         const normalizeResults =  normalizePassengers(rows);
-        console.log(normalizeResults)
         return res.status(200).send(normalizeResults);
     } catch (error) {
         console.log(`Ocurrió un error al obtener los pasajeros con riesgo: ${error}`);
