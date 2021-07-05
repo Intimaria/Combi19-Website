52fab57f88500584754473198420471d8e2cd154
Intimaroia <intimaria@gmail.com>
2021-07-05 15:17:33 -0300

    reports bug fix

--- my-backend/controllers/reports.js
+++ my-backend/controllers/reports.js
@@ -18,8 +18,8 @@ const getRiskyPassengers = async (req, res) => {
                 USER_ID=ID_USER
                 LEFT JOIN DOCUMENT_TYPE D ON U.ID_DOCUMENT_TYPE=DOCUMENT_TYPE_ID
                 WHERE R.ID_ROLE = 3
-                AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -1 MONTH) <= NOW()
-                AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -1 MONTH) >= DATE_ADD(NOW(), INTERVAL -1 MONTH)
+                AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -14 day) <= NOW()
+                AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -14 day) >= DATE_ADD(NOW(), INTERVAL -1 MONTH)
                 ORDER BY U.EXPIRATION_RISK
                 `;
           // AND DATE_ADD(U.EXPIRATION_RISK, INTERVAL -1 MONTH) >= DATE_ADD(NOW(), INTERVAL -1 MONTH)
